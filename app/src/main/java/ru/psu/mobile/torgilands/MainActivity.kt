96e9a3668f1ff45f76c92b71d08e02014437000e
iseletkov <iseletkov@gmail.com>
2023-10-18 21:20:16 +0500

    Пример формирования сообщения. Пример обработки коллекций в функциональном стиле.

--- app/src/main/java/ru/psu/mobile/torgilands/MainActivity.kt
+++ app/src/main/java/ru/psu/mobile/torgilands/MainActivity.kt
@@ -2,13 +2,52 @@ package ru.psu.mobile.torgilands
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
+fun test(
+    a1 : String,
+    a2 : String = "hhhhhh"
+) : String
+{
+    return a1+a2
+}
 
 class MainActivity : AppCompatActivity() {
+    var f : Toast?
+        = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val x= 0
+        val x= 0.0
         val y = 123
+        var z = 345
+        var str = "adawdawdawd"
+        var name = "Иван"
+        var str2 = ""
+
+        var lst1 = listOf("awd", "adawd", "adawdsfsef")
+        var lst2 = mutableListOf("awd", null, "adawd", "gggggg", null, "adawdsfsef", "ffffff")
+
+        val message = lst2
+            .filterNotNull()
+            .filter {
+                it.startsWith("a") }
+            .map {
+                "123$it"
+            }
+            .joinToString(separator = "; ")
+
+
+        Toast.makeText(
+            this,
+            message,
+            Toast.LENGTH_LONG
+        ).show()
+
+
+
     }
 }
